e952bb14b17d5af054ed6696540aaa2642b20487
liupan163 <18792418363@163.com>
2018-11-17 10:35:07 +0800

    upd |> update code

---
+++ dataStructure/LinkedList/QueryUpdateLinkedList.java
@@ -0,0 +1,115 @@
+package LinkedList;
+
+public class QueryUpdateLinkedList<E> {
+    public class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public QueryUpdateLinkedList() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failure, index is out of range");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < size; i++) {
+            prev = prev.next;
+        }
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failure, index is out of range");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < size; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failure, index is out of range");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        /*
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            sb.append(cur + "->");
+            cur = cur.next;
+        }*/
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next) {
+            sb.append(cur + "->");
+        }
+        sb.append("Null");
+        return sb.toString();
+    }
+}
