a85a94ca3c227187e2b4fe2f9c77db10c58d8c63
liupan <18792418363@163.com>
2018-11-18 22:28:51 +0800

    upd |> update code

---
+++ dataStructure/Recursion/Solution.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class Solution{
+
+    public ListNode removeElements(ListNode head,int val) {
+        while(head != null && head.val == val){
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if(head == null)return head;
+
+        ListNode prev = head;
+        while(prev.next != null){
+            if(prev.next.val == val){
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            }else{
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+    public ListNode removeElements(ListNode head, int val, int depth){
+        String depthString = generateDepthString(depth);
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
+
+        if(head == null){
+            System.out.print(depthString);
+            System.out.println("Return: "+ head);
+            return head;
+        }
+        ListNode res = removeElements(head.next,val,depth+1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val +":" + res);
+
+        ListNode ret;
+        if(head.val == val){
+            ret = res;
+        }else{
+            head.next = res;
+            ret = head;
+        }
+        System.out.print(depthString);
+        System.out.println("Return: " + ret);
+
+        return ret;
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i< depth;i++){
+            sb.append("--");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5,6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution()).removeElements(head,6);
+        System.out.println(res);
+    }
+}
