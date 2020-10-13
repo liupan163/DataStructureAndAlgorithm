4ca12c2469e0529bf87625bc391d8513aace77f3
liupan163 <18792418363@163.com>
2020-10-13 17:13:58 +0800

    upd |> update code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DataStructureAndAlgorithm
+
+go 1.14
---
+++ test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+func main() {
+	//println("result bool is ======>", isAnagram("anagram", "nagaram"))
+	//println("result string is ======>", removeDuplicates("bbaacaab"))
+	println("result string is ======>", groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
+
+}
+
+func groupAnagrams(strs []string) [][]string {
+	var strLength = len(strs)
+	var resultMap = make(map[string][]string, strLength)
+	for _, value := range strs {
+		var tempValue = strings.Split(value, "")
+		sort.Strings(tempValue)
+		println("add value  ===>" + value)
+		if _, ok := resultMap[strings.Join(tempValue, "")]; !ok {
+			resultMap[strings.Join(tempValue, "")] = []string{value}
+		} else {
+			resultMap[strings.Join(tempValue, "")] = append(resultMap[strings.Join(tempValue, "")], value)
+		}
+	}
+	var resultList = make([][]string, 0)
+	for _, value := range resultMap {
+		if value==nil{
+			println("resultMap value is nil===>")
+			continue
+		}
+		println(len(value))
+		var rowList = make([]string,0)
+		for _, wordValue := range value {
+			if wordValue==""{
+				continue
+			}
+			rowList = append(rowList, wordValue)
+			println("wordValue===>" + wordValue + " || rowList===>" + strings.Join(rowList, ""))
+		}
+		resultList = append(resultList,rowList)
+	}
+	println(len(resultList))
+	return resultList
+}
+
+func removeDuplicates(S string) string {
+	var tempString = strings.Split(S, "")
+	var isFinish = false
+	for {
+		isFinish = true
+		for index, value := range tempString {
+			if index+1 <= len(tempString)-1 && value == tempString[index+1] {
+				if index-1 < 0 { //	0下标位，0和1位
+					tempString = tempString[index+2:]
+				} else {
+					tempString = append(tempString[:index], tempString[index+2:]...)
+				}
+				isFinish = false
+				break
+			}
+			println("tempString is :" + strings.Join(tempString, ""))
+		}
+		if isFinish {
+			break
+		}
+	}
+	return strings.Join(tempString, "")
+}
+
+func isAnagram(s string, t string) bool {
+	if len(t) != len(s) {
+		return false
+	}
+	var symbolMap = make(map[int]int32, len(s))
+	for index, value := range t {
+		symbolMap[index] = value
+	}
+	for _, value := range s {
+		for key, mapValue := range symbolMap {
+			if mapValue == value {
+				delete(symbolMap, key)
+				break
+			}
+		}
+	}
+	if len(symbolMap) > 0 {
+		return false
+	}
+	return true
+}
