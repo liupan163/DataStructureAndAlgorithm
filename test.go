4873a5fc16558c16736b41ce797b536f3152fd94
parker <18792418363@163.com>
2020-10-21 23:09:35 +0800

    upd |> update code

--- test.go
+++ test.go
@@ -64,6 +64,20 @@ func main() {
 	println("checkAroundPositions is ======>", checkAroundPositions([]int{26, 78, 27, 100, 33, 67, 90, 23, 66, 5, 38, 7, 35, 23, 52, 22, 83, 51, 98, 69, 81, 32, 78, 28, 94, 13, 2, 97, 3, 76, 99, 51, 9, 21, 84, 66, 65, 36, 100, 41})[3])
 }
 
+func findCircleNum(M [][]int) int {
+	var result = 0
+	for x := 0; x < len(M); x++ {
+		for y := 0; y < len(M[0]); y++ {
+			if M[x][y] == 1 {
+				result = result + 1
+				checkFriend(M, x, y)
+			}
+		}
+	}
+	return result
+}
+
+
 type KthLargest struct {
 	capacity int
 	data     []int
